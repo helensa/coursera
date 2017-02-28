22a30e30fd07d41d66a262932e2cea9342e76c29
helensa <helenjsandler@gmail.com>
2017-02-27 21:20:56 -0600

    started building and house

--- Android/m7-assignment-buildings-skeleton/app/src/main/java/mooc/vandy/java4android/buildings/logic/Building.java
+++ Android/m7-assignment-buildings-skeleton/app/src/main/java/mooc/vandy/java4android/buildings/logic/Building.java
@@ -4,7 +4,57 @@ package mooc.vandy.java4android.buildings.logic;
  * This is the Building class file.
  */
 public class Building {
-    
-    // TODO - Put your code here.
-    
+    private int length;
+    private int width;
+    private int lotLength;
+    private int lotWidth;
+
+    public Building(int length, int width, int lotLength, int lotWidth) {
+        this.length = length;
+        this.width = width;
+        this.lotLength = lotLength;
+        this.lotWidth = lotWidth;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLotLength() {
+        return lotLength;
+    }
+
+    public void setLotLength(int lotLength) {
+        this.lotLength = lotLength;
+    }
+
+    public int getLotWidth() {
+        return lotWidth;
+    }
+
+    public void setLotWidth(int lotWidth) {
+        this.lotWidth = lotWidth;
+    }
+
+    @Override
+    public String toString() {
+        return "Building{" +
+                "length=" + length +
+                ", width=" + width +
+                ", lotLength=" + lotLength +
+                ", lotWidth=" + lotWidth +
+                '}';
+    }
 }
--- Android/m7-assignment-buildings-skeleton/app/src/main/java/mooc/vandy/java4android/buildings/logic/House.java
+++ Android/m7-assignment-buildings-skeleton/app/src/main/java/mooc/vandy/java4android/buildings/logic/House.java
@@ -3,9 +3,39 @@ package mooc.vandy.java4android.buildings.logic;
 /**
  * This is the House class file that extends Building.
  */
-public class House 
-       extends Building {
-       
-    // TODO - Put your code here.
-    
+public class House extends Building {
+
+    private  String owner;
+    private boolean pool;
+
+
+    public House(int length, int width, int lotLength, int lotWidth) {
+        this(length, width, lotLength, lotWidth, null);
+    }
+
+    public House(int length, int width, int lotLength, int lotWidth, String owner){
+        this(length, width, lotLength, lotWidth, owner, false);
+    }
+
+    public House(int length, int width, int lotLength, int lotWidth, String owner, boolean pool) {
+        super(length, width, lotLength, lotWidth);
+        this.owner = owner;
+        this.pool = pool;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public boolean hasPool() {
+        return pool;
+    }
+
+    public void setPool(boolean pool) {
+        this.pool = pool;
+    }
 }
